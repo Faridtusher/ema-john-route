d9985831a55cc0b774e0a9bd0db60adbb838697d
farid123d <117964049+farid123d@users.noreply.github.com>
2024-08-21 12:28:58 +0800

    update-data

--- src/components/Inventory/Inventory.jsx
+++ src/components/Inventory/Inventory.jsx
@@ -5,7 +5,7 @@ import './Inventory.css'
 const Inventory = () => {
    return (
       <div>
-         <h1 className='coffee-break'>Hi, I am <span>Ema-John</span> who is tired!! Give me a rest...<span>Haaaaa...</span></h1>
+         <h1 className='coffee-break'>Hi, I am <span>Md Farid</span> who is tired!! Give me a rest...<span>Haaaaa...</span></h1>
          <h2 className='coffee-break'>After taking a <span>coffee</span>, We are coming.....</h2> 
       </div>
    );
