d4a5273d79c9be59c0ebaa5af9e3904461c6c24f
farid123d <117964049+farid123d@users.noreply.github.com>
2024-08-21 14:36:25 +0800

    chnage-logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import './Header.css';
-import logo from '../../images/Logo.svg'
+import logo from '../../images/logo.png'
 import { Link } from 'react-router-dom';
 
 const Header = () => {
